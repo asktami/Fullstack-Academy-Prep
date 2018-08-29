7a989507b4f796a6c9882e24fc97fb363bfd1743
Tami Williams <tami@asktami.com>
2018-08-29 18:31:06 -0400

    cleaned up code

--- 05-arrays-i/05-my-unshift/my-unshift.js
+++ 05-arrays-i/05-my-unshift/my-unshift.js
@@ -1,17 +1,17 @@
 // ### My Unshift
 // Write a function `myUnshift` that takes an array and a value of any type as arguments.
 
-// `myUnshift` should return a new array, with the given value as the first element
-// in the new array.
+// `myUnshift` should return a new array, with the given value as the first element in the new array.
 
 // myUnshift([1, 2, 3], 0); / => [0, 1, 2, 3];
 
 // Do not use the built-in .shift method for this problem!
 
-
 // YOUR CODE BELOW
 const myUnshift = (arr, val) => {
+    // copy the original array
     let newArr = arr.slice();
+    // add val to the beginning
     newArr.unshift(val);
     return newArr;
 }
@@ -29,7 +29,7 @@ const myUnshiftAlt1 = (anArr, val) => {
 }
 
 
-// ALTERNATIVE
+// BEST
 // use .concat() to take 1 array and append another onto the end
 const myUnshiftAlt2 = (anArr, val) => [val].concat(anArr) ;
 
