eef411b899cb87b026c7b664d14f8fcd0139e8f2
Tami Williams <tami@asktami.com>
2018-08-29 19:18:27 -0400

    added solution using reduce

--- 06-arrays-ii/02-array-flattener/array-flattener.js
+++ 06-arrays-ii/02-array-flattener/array-flattener.js
@@ -1,51 +1,39 @@
 // ### Array Flattener
 
-// Define a function, `arrayFlattener`, that accepts a two-dimensional array as an
-// argument.
+// Define a function, `arrayFlattener`, that accepts a two-dimensional array as an argument.
 
 // `arrayFlattener` should return a new, one-dimensional array.
 
 // arrayFlattener([1,[2, 3], 4]); // => [1, 2, 3, 4]
 
-
 // YOUR CODE BELOW
-// const arrayFlattenerAlt = (twoDimensionalArray) => {
-//     let flatArray = [];
+const arrayFlattenerALT = (twoDimensionalArray) => {
+    let flatArray = [];
 
-//     for(let i = 0 ; i < twoDimensionalArray.length; i++) {
+    for(let i = 0 ; i < twoDimensionalArray.length; i++) {
 
-//         let element =  twoDimensionalArray[i];
+        let element =  twoDimensionalArray[i];
 
-//         if( Array.isArray(element) ) {
+        if( Array.isArray(element) ) {
 
-//             for(let j = 0 ; j < element.length; j++) {
-//                 let innerElement = element[j];
+            for(let j = 0 ; j < element.length; j++) {
+                let innerElement = element[j];
 
-//                 flatArray.push(innerElement) ;
-//             }
+                flatArray.push(innerElement) ;
+            }
 
-//         } else {
-//             flatArray.push(element) ;
-//         }
-//     }
+        } else {
+            flatArray.push(element) ;
+        }
+    }
 
-//     return flatArray;
-// }
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
+    return flatArray;
+}
+
  
 
 // ALTERNATIVE using concat
-const arrayFlattener = (twoDimensionalArray) => {
+const arrayFlattenerALT2 = (twoDimensionalArray) => {
     let flatArray = [];
 
     for(let i = 0 ; i < twoDimensionalArray.length; i++) {
@@ -59,6 +47,24 @@ const arrayFlattener = (twoDimensionalArray) => {
             flatArray.push(element) ;
         }
     }
+
+    return flatArray;
+}
+
+
+// BEST
+const arrayFlattener = (twoDimensionalArray) => {
+
+    let flatArray = twoDimensionalArray.reduce((acc, element) => { 
+        if(Array.isArray(element)) {
+            acc = acc.concat(element) ;
+        } else {
+            acc.push(element) ;
+        }
+        
+    return acc;
+
+    }, []);
 
     return flatArray;
 }
