c246fcd2dc100795dec06d3bec8aea0985c368e7
Tami Williams <tami@asktami.com>
2018-08-29 18:49:37 -0400

    updated code

--- 06-arrays-ii/01-even-and-odd/even-and-odd.js
+++ 06-arrays-ii/01-even-and-odd/even-and-odd.js
@@ -2,16 +2,12 @@
 
 // Define a function, `evenAndOdd`, that accepts an array.
 
-// `evenAndOdd` should return a new array. The first element in the new array
-// should be an array with all of the even numbers from the original array. The
-// second element in the new array should be an array with all of the odd numbers
-// from the original array.
+// `evenAndOdd` should return a new array. The first element in the new array should be an array with all of the even numbers from the original array. The second element in the new array should be an array with all of the odd numbers from the original array.
 
 // evenAndOdd([1, 2, 3, 4, 5, 6]); // => [[ 2 ,4, 6 ], [ 1, 3, 5 ]]
 
 // YOUR CODE BELOW
-const evenAndOdd = anArray => {
-  debugger;
+const evenAndOddALT = anArray => {
   const evenArray = [];
   const oddArray = [];
 
@@ -25,3 +21,12 @@ const evenAndOdd = anArray => {
 
   return [evenArray, oddArray];
 };
+
+
+// BEST
+const evenAndOdd = anArray => {
+  let evenArray = anArray.filter(element => element % 2 === 0);
+  let oddArray = anArray.filter(element => element % 2 === 1);
+
+  return[evenArray, oddArray];
+}
